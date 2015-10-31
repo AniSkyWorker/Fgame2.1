edb0bc30d5cf839e4f5f51d91f06831000d78fa0
AnySkyWorker <holyrecycler@mail.ru>
2015-10-31 20:51:48 +0300

    Changed declaration and names of cnonstants.

--- Fgame2.1/Fgame2.1.cpp
+++ Fgame2.1/Fgame2.1.cpp
@@ -5,24 +5,36 @@
 using namespace sf;
 using namespace std;
 
-const int window_width = 800, window_heigth = 600;
-const int count_blocks_x = 10, count_blocks_y = 4;
-const float ball_radius = 10, ball_speed = 6;
-const float paddle_width = 120, paddle_height = 10, paddle_speed = 12;
-const float block_width = 60, block_height = 20, range_beetwen_blocks = 5;
-const float x_coefficient = 1.75, y_coefficient = 2;
-const int button_number = 2, messages_number = 2;
-const string play = "Play", win = "You win!", lost = "You Lost!", escape = "Exit";
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const int COUNT_BLOCKS_X = 10;
+const int COUNT_BLOCKS_Y = 4;
+const float BALL_RADIUS = 10;
+const float BALL_SPEED = 6;
+const float PADDLE_WIDTH = 120;
+const float PADDLE_HEIGHT = 10;
+const float PADDLE_SPEED = 12;
+const float BLOCK_WIDTH = 60;
+const float BLOCK_HEIGHT = 20;
+const float RANGE_BEETWEN_BLOCKS = 5;
+const float X_COEFFICIENT = 1.75;
+const float Y_COEFFICIENT = 2;
+const int BUTTONS_NUMBER = 2;
+const int MESSAGES_NUMBER = 2;
+const string PLAY_BUTTON = "Play";
+const string WIN_BUTTON = "You WIN!";
+const string LOST_BUTTON = "You Lost!";
+const string ESCAPE_BUTTON = "Exit";
 
 struct ball {
 	CircleShape shape;
-	Vector2f ballspeed{ -ball_speed, -ball_speed };
+	Vector2f ballspeed{ -BALL_SPEED, -BALL_SPEED };
 
 	ball(float X, float Y) {
 		shape.setPosition(X, Y);
-		shape.setRadius(ball_radius);
+		shape.setRadius(BALL_RADIUS);
 		shape.setFillColor(Color::Red);
-		shape.setOrigin(ball_radius, ball_radius);
+		shape.setOrigin(BALL_RADIUS, BALL_RADIUS);
 	}
 
 	float x() {
@@ -48,11 +60,11 @@ struct ball {
 		shape.move(ballspeed);
 		
 		if (left_side() < 0)
-			ballspeed.x = ball_speed;
-		else if (right_side() > window_width)
-			ballspeed.x = -ball_speed;
+			ballspeed.x = BALL_SPEED;
+		else if (right_side() > WINDOW_WIDTH)
+			ballspeed.x = -BALL_SPEED;
 		if (top() < 0)
-			ballspeed.y = ball_speed;
+			ballspeed.y = BALL_SPEED;
 	}
 };
 struct paddle {
@@ -61,9 +73,9 @@ struct paddle {
 
 	paddle(float X, float Y){
 		shape.setPosition(X, Y);
-		shape.setSize(Vector2f(paddle_width, paddle_height));
+		shape.setSize(Vector2f(PADDLE_WIDTH, PADDLE_HEIGHT));
 		shape.setFillColor(Color::Blue);
-		shape.setOrigin(paddle_width / 2, paddle_height / 2);
+		shape.setOrigin(PADDLE_WIDTH / 2, PADDLE_HEIGHT / 2);
 	}
 
 	float x() {
@@ -89,9 +101,9 @@ struct paddle {
 		shape.move(paddlespeed);
 
 		if ((Keyboard::isKeyPressed(Keyboard::Key::Left) || Keyboard::isKeyPressed(Keyboard::Key::A)) && left_side() > 0)
-			paddlespeed.x = -paddle_speed;
-		else if ((Keyboard::isKeyPressed(Keyboard::Key::Right) || Keyboard::isKeyPressed(Keyboard::Key::D)) && right_side() < window_width)
-			paddlespeed.x = paddle_speed;
+			paddlespeed.x = -PADDLE_SPEED;
+		else if ((Keyboard::isKeyPressed(Keyboard::Key::Right) || Keyboard::isKeyPressed(Keyboard::Key::D)) && right_side() < WINDOW_WIDTH)
+			paddlespeed.x = PADDLE_SPEED;
 		else
 			paddlespeed.x = 0;
 	}
@@ -105,12 +117,12 @@ void setCollisionPaddle(ball& cB, paddle& cP) {
 	if (!checkCollision(cB, cP)) 
 		return;
 
-	cB.ballspeed.y = -ball_speed;
+	cB.ballspeed.y = -BALL_SPEED;
 
 	if (cB.x() < cP.x())
-		cB.ballspeed.x = -ball_speed;
+		cB.ballspeed.x = -BALL_SPEED;
 	else
-		cB.ballspeed.x = ball_speed;
+		cB.ballspeed.x = BALL_SPEED;
 }
 
 struct brick {
@@ -120,9 +132,9 @@ struct brick {
 
 	brick(float X, float Y) {
 		shape.setPosition(X, Y);
-		shape.setSize(Vector2f(block_width, block_height));
+		shape.setSize(Vector2f(BLOCK_WIDTH, BLOCK_HEIGHT));
 		shape.setFillColor(Color::Green);
-		shape.setOrigin(block_width / 2, block_height / 2);
+		shape.setOrigin(BLOCK_WIDTH / 2, BLOCK_HEIGHT / 2);
 	}
 
 
@@ -147,9 +159,9 @@ struct brick {
 };
 
 vector<Vector2f> emplace_bricks(vector<Vector2f> bricks) {
-	for (int i(0); i < count_blocks_x; ++i)
-		for (int j(0); j < count_blocks_y; ++j)
-			bricks.emplace_back((i + x_coefficient) * (block_width + range_beetwen_blocks), (j + y_coefficient) * (block_height + range_beetwen_blocks));
+	for (int i(0); i < COUNT_BLOCKS_X; ++i)
+		for (int j(0); j < COUNT_BLOCKS_Y; ++j)
+			bricks.emplace_back((i + X_COEFFICIENT) * (BLOCK_WIDTH + RANGE_BEETWEN_BLOCKS), (j + Y_COEFFICIENT) * (BLOCK_HEIGHT + RANGE_BEETWEN_BLOCKS));
 	return bricks;
 } //функция не работает в релизной версии
 void setCollisionBr(ball& cB, brick& cBr) {
@@ -170,9 +182,9 @@ void setCollisionBr(ball& cB, brick& cBr) {
 	float overlap_y = collision_top ? coord_diff_top : coord_diff_bottom;
 
 	if (overlap_x < overlap_y)
-		cB.ballspeed.x = collision_left ? -ball_speed : ball_speed;
+		cB.ballspeed.x = collision_left ? -BALL_SPEED : BALL_SPEED;
 	else
-		cB.ballspeed.y = collision_top ? -ball_speed : ball_speed;
+		cB.ballspeed.y = collision_top ? -BALL_SPEED : BALL_SPEED;
 }
 
 struct sound {
@@ -202,20 +214,20 @@ struct sound {
 struct menu {
 	int selected_item;
 	Font font;
-	Text text[button_number];
+	Text text[BUTTONS_NUMBER];
 	
 	menu() {
 		font.loadFromFile("arialn.ttf");
 
 		text[0].setFont(font);
 		text[0].setColor(Color::Red);
-		text[0].setString(play);
-		text[0].setPosition(Vector2f(window_width / 2, window_heigth / 3));
+		text[0].setString(PLAY_BUTTON);
+		text[0].setPosition(Vector2f(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 3));
 
 		text[1].setFont(font);
 		text[1].setColor(Color::White);
-		text[1].setString(escape);
-		text[1].setPosition(Vector2f(window_width / 2, window_heigth / 2));
+		text[1].setString(ESCAPE_BUTTON);
+		text[1].setPosition(Vector2f(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2));
 
 		selected_item = 0;
 	}
@@ -228,7 +240,7 @@ struct menu {
 		}
 	}
 	void MoveDown() {
-		if (selected_item + 1 < button_number) {
+		if (selected_item + 1 < BUTTONS_NUMBER) {
 			text[selected_item].setColor(Color::White);
 			selected_item++;
 			text[selected_item].setColor(Color::Red);
@@ -238,22 +250,22 @@ struct menu {
 
 struct message {
 	Font font;
-	Text messages[messages_number];
+	Text messages[MESSAGES_NUMBER];
 
 	message() {
 		font.loadFromFile("arialn.ttf");
 
 		messages[0].setFont(font);
 		messages[0].setColor(Color::White);
-		messages[0].setString(win);
+		messages[0].setString(WIN_BUTTON);
 		messages[0].setCharacterSize(50);
-		messages[0].setPosition(Vector2f(window_width / 2, window_heigth / 2));
+		messages[0].setPosition(Vector2f(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2));
 
 		messages[1].setFont(font);
 		messages[1].setColor(Color::Red);
-		messages[1].setString(lost);
+		messages[1].setString(LOST_BUTTON);
 		messages[1].setCharacterSize(50);
-		messages[1].setPosition(Vector2f(window_width / 2, window_heigth / 2));
+		messages[1].setPosition(Vector2f(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2));
 
 	}
 };
@@ -263,21 +275,21 @@ int main() {
 	music.openFromFile("13401934206065.wav");
 	music.play();
 
-	ball Ball(window_width / 2.f, window_heigth / 2.f);
-	paddle Paddle(window_width / 2.f, window_heigth - 10);
+	ball Ball(WINDOW_WIDTH / 2.f, WINDOW_HEIGHT / 2.f);
+	paddle Paddle(WINDOW_WIDTH / 2.f, WINDOW_HEIGHT - 10);
 	sound Sound;
 	menu Menu;
 	message Message;
-	//vector<Vector2f> bricks = emplace_bricks(bricks); для версии debug
-	vector<Vector2f> bricks; 
-		for (int i(0); i < count_blocks_x; ++i)
-			for (int j(0); j < count_blocks_y; ++j)
-				bricks.emplace_back((i + x_coefficient) * (block_width + range_beetwen_blocks), (j + y_coefficient) * (block_height + range_beetwen_blocks));
+	vector<Vector2f> bricks = emplace_bricks(bricks);
+	/*vector<Vector2f> bricks; 
+		for (int i(0); i < COUNT_BLOCKS_X; ++i)
+			for (int j(0); j < COUNT_BLOCKS_Y; ++j)
+				bricks.emplace_back((i + X_COEFFICIENT) * (BLOCK_WIDTH + RANGE_BEETWEN_BLOCKS), (j + X_COEFFICIENT) * (BLOCK_HEIGHT + RANGE_BEETWEN_BLOCKS));*/
 	//^для release
 	bool pause = false;
 	bool choose = true;
 
-	RenderWindow window(VideoMode(window_width, window_heigth), "Arcanoid");
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Arcanoid");
 	window.setFramerateLimit(50);
 	while (window.isOpen()) {
 
@@ -323,7 +335,7 @@ int main() {
 		}
 		window.clear(Color::Black);
 		if (choose == true) {
-			for (int i(0); i < button_number; ++i)
+			for (int i(0); i < BUTTONS_NUMBER; ++i)
 				window.draw(Menu.text[i]);
 		}
 		else {
@@ -335,13 +347,13 @@ int main() {
 					Paddle.update();
 				}
 				setCollisionPaddle(Ball, Paddle);
-				if ((Ball.y() < window_heigth) && (bricks.size() != 0)) {
+				if ((Ball.y() < WINDOW_HEIGHT) && (bricks.size() != 0)) {
 					window.draw(Paddle.shape);
 					window.draw(Ball.shape);
 					Sound.drawbricks(window, Ball, bricks);
 				}
 				else if (bricks.size() == 0) window.draw(Message.messages[0]);
-				else if (Ball.y() > window_heigth) window.draw(Message.messages[1]);
+				else if (Ball.y() > WINDOW_HEIGHT) window.draw(Message.messages[1]);
 				break;
 
 			case 1:
